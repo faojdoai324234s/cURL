f22d3f1fa61aa0b362a01149f6c2d12387efc571
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-09 20:00:53 +0000

    Update build_win.bat

--- build_win.bat
+++ build_win.bat
@@ -9,8 +9,9 @@ mkdir upload\Release
 REM Download latest libcurl
 git clone --branch curl-8_12_1 --single-branch https://github.com/curl/curl
 
-REM Install zlib
+REM Install dependencies
 vcpkg install zlib
+vcpkg install brotli
 
 REM Build Debug configuration
 cmake -S curl -B build -D CMAKE_BUILD_TYPE=Debug
